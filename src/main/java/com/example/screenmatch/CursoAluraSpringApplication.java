8cfa343a2354b71108046c462f2a1f2ddfd477a8
Eduardo Dos Santos Fritzen <eduardo.fritzen@3035tech.com>
2025-10-14 15:50:46 -0300

    Update CursoAluraSpringApplication.java, removing imports

--- src/main/java/com/example/screenmatch/CursoAluraSpringApplication.java
+++ src/main/java/com/example/screenmatch/CursoAluraSpringApplication.java
@@ -1,9 +1,6 @@
 package com.example.screenmatch;
 
 import com.example.screenmatch.main.Main;
-import com.example.screenmatch.model.SeriesData;
-import com.example.screenmatch.service.ApiConsumption;
-import com.example.screenmatch.service.DataConverter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
