afe59625f01c79c22d797cbc33f41c20cd724ace
Eduardo Dos Santos Fritzen <eduardo.fritzen@3035tech.com>
2025-10-14 15:49:15 -0300

    feat: lesson 4
    Search complete series data

---
+++ src/main/java/com/example/screenmatch/main/Main.java
@@ -0,0 +1,41 @@
+package com.example.screenmatch.main;
+
+import com.example.screenmatch.model.SeasonData;
+import com.example.screenmatch.model.SeriesData;
+import com.example.screenmatch.service.ApiConsumption;
+import com.example.screenmatch.service.DataConverter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    private Scanner scanner = new Scanner(System.in);
+
+    private ApiConsumption consumption = new ApiConsumption();
+    private DataConverter converter = new DataConverter();
+
+    private final String ADDRESS = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=f4db69c5";
+    private final String SEASON = "&season=";
+    public void showMenu() {
+        System.out.println("Digite o nome da série para buscar: ");
+
+        var seriesName =  scanner.nextLine();
+        var json = consumption.getData(ADDRESS + seriesName.replace(" ", "+") + API_KEY);
+
+        SeriesData seriesData = converter.getData(json, SeriesData.class);
+        System.out.println(seriesData);
+
+        List<SeasonData> seasons = new ArrayList<>();
+
+        for (int l = 1; l < seriesData.totalSeasons(); l++){
+            json = consumption.getData(ADDRESS + seriesName.replace(" ", "+") + SEASON + l + API_KEY);
+            SeasonData seasonData = converter.getData(json, SeasonData.class);
+
+            seasons.add(seasonData);
+        }
+
+        seasons.forEach(System.out::println);
+    }
+}
---
+++ src/main/java/com/example/screenmatch/model/EpisodeData.java
@@ -0,0 +1,13 @@
+package com.example.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record EpisodeData(
+        @JsonAlias("Title") String title,
+        @JsonAlias("Episode") Integer episodeNumber,
+        @JsonAlias("imdbRating") String imdbRating,
+        @JsonAlias("Released") String releasedDate
+) {
+}
