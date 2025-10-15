381233b6a942df9d97b39794e926f171731f939d
Eduardo Dos Santos Fritzen <eduardo.fritzen@3035tech.com>
2025-10-15 11:14:02 -0300

    Merge pull request #2 from eduardo-fritzen/feature/modeling-series-data
    Feature/modeling series data

--- src/main/java/com/example/screenmatch/CursoAluraSpringApplication.java
+++ src/main/java/com/example/screenmatch/CursoAluraSpringApplication.java
@@ -1,8 +1,6 @@
 package com.example.screenmatch;
 
-import com.example.screenmatch.model.SeriesData;
-import com.example.screenmatch.service.ApiConsumption;
-import com.example.screenmatch.service.DataConverter;
+import com.example.screenmatch.main.Main;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,13 +14,7 @@ public class CursoAluraSpringApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        var consumoApi = new ApiConsumption();
-        var json = consumoApi.getData("https://www.omdbapi.com/?t=the+big+bang+theory&apikey=f4db69c5");
-        System.out.println(json);
-
-        DataConverter converter = new DataConverter();
-        SeriesData seriesData = converter.getData(json, SeriesData.class);
-        System.out.println(seriesData);
-        System.out.println("Hello World");
+        Main main = new Main();
+        main.showMenu();
     }
 }
